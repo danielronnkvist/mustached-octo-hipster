c619593833ea6d10845a9d3a800b91110473915b
danielronnkvist <daniel@ronnkvist.com>
2015-02-17 17:23:01 +0100

    Implemented express

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var http = require('http');
 var sys = require('sys');
 var exec = require('child_process').exec;
+var express = require('express')
+var app = express();
+var ejs = require('ejs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+app.engine('html', ejs.renderFile);
 
-  function puts(error, stdout, stderr) { sys.puts(stdout) }
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+app.get('/photo', function(req,res){
+  function puts(error, stdout, stderr) { sys.puts(stdout); }
   exec("node -v", puts);
-  res.end('Hello World\n');
-}).listen(8000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8000/');
+});
+
+var server = app.listen(8000, function() {
+  console.log("Listening on port %d", server.address().port);
+});
+
