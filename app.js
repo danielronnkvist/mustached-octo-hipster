3bb32565db597afe8156f8e8806820793a84418b
danielronnkvist <daniel@ronnkvist.com>
2015-02-18 16:21:11 +0100

    GM stuff is nice stuff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express();
 var ejs = require('ejs');
 var fs = require('fs');
-var gm = require('gm');
+var photos = [];
 
 app.engine('html', ejs.renderFile);
 // Static folder with resources
@@ -18,10 +18,21 @@ app.get('/', function(req, res){
 app.get('/photo', function(req,res){
   function puts(error, stdout, stderr) {
     console.log(stdout);
-    // Make some sweeet image magick stuff
-    return res.end("DATA")
+    // Make some sweeet GraphicsMagick stuff
+    fs.readdir('/pictures', function(err, images) {
+      var s = "gm montage -geometry 2048x1300 ";
+      for(var i = 0; i < 3; i++){
+        s += images[i] + " ";
+      }
+      var filename = photos.length + ".jpg";
+      photos.push(filename);
+      s += "pictures/"+filename;
+      exec(s, function(err, stdout, stderr){
+        return res.end(filename)
+      });
+    });
   }
-  exec("gphoto2 --capture-image-and-download --frames 4 --filename pictures/%Y%m%d%H%M%S.%C ", puts);
+  exec("gphoto2 --capture-image-and-download --frames 3 --filename pictures/%Y%m%d%H%M%S.%C ", puts);
 });
 
 var server = app.listen(8000, function() {
