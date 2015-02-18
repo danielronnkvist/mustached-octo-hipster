58ca84d398c27dd0de64031615e7f8cc286c99d3
danielronnkvist <daniel@ronnkvist.com>
2015-02-18 08:44:17 +0100

    gphoto2 system executions

--- app.js
+++ app.js
@@ -11,15 +11,22 @@ app.get('/', function(req, res){
   res.render('index.html');
 });
 
+
 app.get('/photo', function(req,res){
   function puts(error, stdout, stderr) {
     sys.puts(stdout);
-    return res.end(stdout)
   }
   exec("ls", puts);
+  exec("gphoto2 --capture-image-and-download", puts);
+  exec("ls", puts);
+  return res.end("lol")
 });
 
 var server = app.listen(8000, function() {
+  function puts(error, stdout, stderr) {
+    sys.puts(stdout);
+  }
+  exec("gphoto2 --auto-detect", puts);
   console.log("Listening on port %d", server.address().port);
 });
 
